3586daacc9c8cef834802a6f261896a351690302
sisir-bhowmick <sisir.bhowmick@accenture.com>
2026-05-11 11:38:25 +0000

    api endpoint changes.

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-@n!-v(255ecs=f-j%y-y36ajjr+2n$1&ug-llp00(%b&1k_2r)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
 
 
 # Application definition
--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from .views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+import os
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -31,13 +33,24 @@ router.register(r'workouts', WorkoutViewSet)
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'teams': reverse('team-list', request=request, format=format),
-        'activities': reverse('activity-list', request=request, format=format),
-        'leaderboard': reverse('leaderboard-list', request=request, format=format),
-        'workouts': reverse('workout-list', request=request, format=format),
-    })
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    if codespace_name:
+        base_url = f"https://{codespace_name}-8000.app.github.dev/api/"
+        return Response({
+            'users': base_url + 'users/',
+            'teams': base_url + 'teams/',
+            'activities': base_url + 'activities/',
+            'leaderboard': base_url + 'leaderboard/',
+            'workouts': base_url + 'workouts/',
+        })
+    else:
+        return Response({
+            'users': request.build_absolute_uri(reverse('user-list')),
+            'teams': request.build_absolute_uri(reverse('team-list')),
+            'activities': request.build_absolute_uri(reverse('activity-list')),
+            'leaderboard': request.build_absolute_uri(reverse('leaderboard-list')),
+            'workouts': request.build_absolute_uri(reverse('workout-list')),
+        })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
